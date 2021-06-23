b503921a704d263d579664b766484047a460ef01
Uddesh Rothe <uddes@LAPTOP-N8UQRCEI>
2021-06-23 16:33:16 +0530

    updated CouponController

--- coupon-service/src/main/java/com/couponsservice/controller/CoController.java
+++ coupon-service/src/main/java/com/couponsservice/controller/CoController.java
@@ -13,7 +13,7 @@ import com.couponsservice.entities.Coupon;
 import com.couponsservice.repository.CouponRepository;
 import com.couponsservice.services.CouponService;
 
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = "http://localhost:8081/")
 @RestController
 @RequestMapping
 public class CoController {
